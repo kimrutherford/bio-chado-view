307a9a97b9093155d46507f3afef5357c28dfc07
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-06-03 22:42:49 +0000

    Summarise first before of raw read files.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@545 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- script/load_static.pl
+++ script/load_static.pl
@@ -344,6 +344,26 @@ my @analyses = (
                     ]
                 },
                 {
+                 type_term_name => 'summarise fasta first base',
+                 runable_name => 'SmallRNA::Runable::FirstBaseCompSummaryRunable',
+                 inputs => [
+                     {
+                       format_type => 'fasta',
+                       content_type => 'raw_small_rna_reads',
+                     }
+                    ]
+                },
+                {
+                 type_term_name => 'summarise fasta first base',
+                 runable_name => 'SmallRNA::Runable::FirstBaseCompSummaryRunable',
+                 inputs => [
+                     {
+                       format_type => 'fasta',
+                       content_type => 'multiplexed_small_rna_reads',
+                     }
+                    ]
+                },
+                {
                  type_term_name => 'remove redundant reads',
                  runable_name => 'SmallRNA::Runable::NonRedundantFastaRunable',
                  inputs => [
