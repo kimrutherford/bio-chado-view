322f095712d586fbcb0848537b9857d58b8623f3
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-06-03 21:50:55 +0000

    Try harder to find the right input file.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@533 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- script/load_booking_sheet.pl
+++ script/load_booking_sheet.pl
@@ -335,7 +335,16 @@ sub find_real_file_name
       return $dir_files{$test_file_name};
     } else {
       warn "can't find file for $booking_sheet_file_name ($test_file_name)\n";
-      return undef;
+
+      $test_file_name =~ s/^SL\d+\.//;
+
+      if (exists $dir_files{$test_file_name}) {
+#      warn "found file: ", $dir_files{$test_file_name}, " - $test_file_name\n";
+        return $dir_files{$test_file_name};
+      } else {
+        warn "can't find file for $booking_sheet_file_name ($test_file_name)\n";
+        return undef;
+      }
     }
   }
 }
